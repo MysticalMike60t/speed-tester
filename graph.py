36ee38cc4357e6ae6d8846b1bd9ac9b6edf23be3
MysticalMike60t <cadenmfink@gmail.com>
2024-07-27 22:54:15 -0400

    Update graph.py

--- graph.py
+++ graph.py
@@ -15,9 +15,29 @@ def read_data(filename='network_speeds.csv'):
 
 def plot_data(timestamps, download_speeds, upload_speeds, pings):
     plt.figure(figsize=(10, 6))
-    plt.plot(timestamps, download_speeds, label='Download Speed (Mbps)')
-    plt.plot(timestamps, upload_speeds, label='Upload Speed (Mbps)')
-    plt.plot(timestamps, pings, label='Ping (ms)')
+    
+    # Plot download and upload speeds
+    plt.plot(timestamps, download_speeds, label='Download Speed (Mbps)', color='blue')
+    plt.plot(timestamps, upload_speeds, label='Upload Speed (Mbps)', color='green')
+    
+    # Process ping values
+    processed_pings = []
+    for i, ping in enumerate(pings):
+        if ping > 150:
+            processed_pings.append(150)
+            plt.annotate(
+                '>',
+                xy=(timestamps[i], 150),
+                xytext=(timestamps[i], 150 + 5),
+                ha='center',
+                color='red'
+            )
+        else:
+            processed_pings.append(ping)
+    
+    # Plot ping values as a line
+    plt.plot(timestamps, processed_pings, label='Ping (ms)', color='red', linestyle='dashed')
+
     plt.xlabel('Time')
     plt.ylabel('Speed')
     plt.title('Network Speeds Over Time')
